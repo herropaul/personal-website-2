d87355cfc1508e6e6c7afbfb4ee863bd8712bacb
Paul Nguon <herropaul@gmail.com>
2022-01-14 12:31:55 -0800

    Added components folder and SideBar component

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,10 +1,10 @@
-
+import SideBar from '../components/SideBar';
 export default function Home () {
   return (
     <div className="h-screen w-screen md:flex">
       
       {/* mobile menu bar */}
-      <div className="bg-gray-500 flex justify-between">
+      <div className="bg-gray-500 flex justify-between md:hidden">
         {/* logo */}
         <a href="#" className="block p-4">
           <h2 className="text-2xl text-gray-100">PN</h2>
@@ -19,21 +19,7 @@ export default function Home () {
       </div>
 
       {/*side bar */}
-      <div className="bg-blue-300 w-32 space-y-6 px-2 absolute inset-y-0 left-0 transform -translate-x-full 
-      transition duration-200 ease-in-out">
-        <a href="#" className="flex items-center space-x-2">
-          <img src="../paul_logo.jpg"/>
-        </a>
-
-        {/* nav */}
-        <nav>
-          <a href="" className="block py-2.5 px-4 rounded transition duration-200 hover:bg-blue-400 hover:text-white">Home</a>
-          <a href="" className="block py-2.5 px-4 rounded transition duration-200 hover:bg-blue-400 hover:text-white">About</a>
-          <a href="" className="block py-2.5 px-4 rounded transition duration-200 hover:bg-blue-400 hover:text-white">Projects</a>
-          <a href="" className="block py-2.5 px-4 rounded transition duration-200 hover:bg-blue-400 hover:text-white">Resume</a> 
-          <a href="" className="block py-2.5 px-4 rounded transition duration-200 hover:bg-blue-400 hover:text-white">Contact</a> 
-        </nav>
-      </div>
+      <SideBar></SideBar>
 
       {/* content */}
       <div className="flex-1 p-20 text-2xl font-bold">
