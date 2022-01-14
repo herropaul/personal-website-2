5b2ed43fd296506ee243282eec41d35d8801e6d5
Paul Nguon <herropaul@gmail.com>
2022-01-13 19:40:55 -0800

    added logo and nav bar contents

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,14 +1,21 @@
-import type { NextPage } from 'next'
 
 export default function Home () {
   return (
     <div className="h-screen w-screen flex">
       {/*side bar */}
-      <div className="bg-blue-300 w-32">
-        logo
+      <div className="bg-blue-300 w-32 space-y-6">
+        <a href="#" className="flex items-center space-x-2">
+          <img src="../paul_logo.jpg"/>
+        </a>
 
         {/* nav */}
-        <nav> nav </nav>
+        <nav>
+          <a href="" className="block py-2 5 px-4">Home</a>
+          <a href="" className="block py-2 5 px-4">About</a>
+          <a href="" className="block py-2 5 px-4">Projects</a>
+          <a href="" className="block py-2 5 px-4">Resume</a> 
+          <a href="" className="block py-2 5 px-4">Contact</a> 
+        </nav>
       </div>
 
       {/* content */}
