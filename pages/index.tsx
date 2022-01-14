61b096fedc18c044a276979ae72bcee715356954
Paul Nguon <herropaul@gmail.com>
2022-01-14 11:57:32 -0800

    mobile menu bar responsive

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,9 +1,26 @@
 
 export default function Home () {
   return (
-    <div className="h-screen w-screen flex">
+    <div className="h-screen w-screen md:flex">
+      
+      {/* mobile menu bar */}
+      <div className="bg-gray-500 flex justify-between">
+        {/* logo */}
+        <a href="#" className="block p-4">
+          <h2 className="text-2xl text-gray-100">PN</h2>
+        </a>
+
+        {/* mobile menu button */}
+        <button className="p-4 focus:outline-none focus:bg-gray-400">
+          <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+            <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M4 6h16M4 12h16M4 18h16" />
+          </svg>
+        </button>
+      </div>
+
       {/*side bar */}
-      <div className="bg-blue-300 w-32 space-y-6 px-2">
+      <div className="bg-blue-300 w-32 space-y-6 px-2 absolute inset-y-0 left-0 transform -translate-x-full 
+      transition duration-200 ease-in-out">
         <a href="#" className="flex items-center space-x-2">
           <img src="../paul_logo.jpg"/>
         </a>
