db42ea81c0a23666bb75b348e8e2aa871ca4a5e5
Paul Nguon <herropaul@gmail.com>
2022-01-13 18:49:35 -0800

    side navbar layout

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,5 +1,20 @@
 import type { NextPage } from 'next'
 
 export default function Home () {
-  return <div>Hello world</div>
+  return (
+    <div className="h-screen w-screen flex">
+      {/*side bar */}
+      <div className="bg-blue-300 w-32">
+        logo
+
+        {/* nav */}
+        <nav> nav </nav>
+      </div>
+
+      {/* content */}
+      <div className="flex-1 p-20 text-2xl font-bold">
+        content
+      </div>
+    </div>
+  );
 }
