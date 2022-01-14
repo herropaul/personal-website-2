1959cd4de0517c7900df607ed295ea675c461f3a
Paul Nguon <herropaul@gmail.com>
2022-01-13 20:22:00 -0800

    styled nav bar

--- pages/index.tsx
+++ pages/index.tsx
@@ -3,18 +3,18 @@ export default function Home () {
   return (
     <div className="h-screen w-screen flex">
       {/*side bar */}
-      <div className="bg-blue-300 w-32 space-y-6">
+      <div className="bg-blue-300 w-32 space-y-6 px-2">
         <a href="#" className="flex items-center space-x-2">
           <img src="../paul_logo.jpg"/>
         </a>
 
         {/* nav */}
         <nav>
-          <a href="" className="block py-2 5 px-4">Home</a>
-          <a href="" className="block py-2 5 px-4">About</a>
-          <a href="" className="block py-2 5 px-4">Projects</a>
-          <a href="" className="block py-2 5 px-4">Resume</a> 
-          <a href="" className="block py-2 5 px-4">Contact</a> 
+          <a href="" className="block py-2.5 px-4 rounded transition duration-200 hover:bg-blue-400 hover:text-white">Home</a>
+          <a href="" className="block py-2.5 px-4 rounded transition duration-200 hover:bg-blue-400 hover:text-white">About</a>
+          <a href="" className="block py-2.5 px-4 rounded transition duration-200 hover:bg-blue-400 hover:text-white">Projects</a>
+          <a href="" className="block py-2.5 px-4 rounded transition duration-200 hover:bg-blue-400 hover:text-white">Resume</a> 
+          <a href="" className="block py-2.5 px-4 rounded transition duration-200 hover:bg-blue-400 hover:text-white">Contact</a> 
         </nav>
       </div>
 
