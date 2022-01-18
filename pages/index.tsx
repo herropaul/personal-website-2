935d80debcaa25b86946b5e0df36cbb8aad43f83
Paul Nguon <herropaul@gmail.com>
2022-01-17 19:58:01 -0800

    added menu button top right

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,9 +1,16 @@
 import SideBar from '../components/SideBar';
 import MobileMenuButton from '../components/MobileMenuButton';
 import {Section, SectionTitle, SectionText} from '../styles/GlobalComponents/index';
+import React, { useState } from 'react';
+
 export default function Home () {
   return (
     <div className="h-screen w-screen md:flex">
+
+      {/*Menu button*/}
+      <div className="absolute top-0 right-0 h-16 w-16">
+        <MobileMenuButton></MobileMenuButton>
+      </div>
       
       {/* mobile menu bar */}
       <div className="bg-gray-500 flex justify-between md:hidden">
