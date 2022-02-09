86168443afb6c88d80529af99ee5dbaff83e36ca
Paul Nguon <herropaul@gmail.com>
2022-02-08 16:07:14 -0800

    updated md responsive text

--- components/Resume/Resume.tsx
+++ components/Resume/Resume.tsx
@@ -14,10 +14,10 @@ export default function Resume() {
                 <GridContainer>
                     {skills.map(({id, title, names}) => (
                         <ul key={id}>
-                            <li className="pb-4">{title}</li>
+                            <li className="pb-3">{title}</li>
                             {names.map((item, i) => (
                                 <ul key={i}>
-                                    <li className="text-base font-sans">
+                                    <li className="text-base text-gray-300 font-light md:text-lg">
                                         {item}
                                     </li>
                                 </ul>
@@ -28,7 +28,7 @@ export default function Resume() {
                 <section className="pt-8 grid grid-cols-2 gap-x-14">
                     <div>
                         Education
-                        <div className="pt-4 text-sm text-gray-300">
+                        <div className="pt-4 text-sm text-gray-300 md:text-base">
                             <p>Graduating May 2022</p>
                             <p className="pt-3 font-light">California State University of Long Beach</p>
                             <p className="pt-3 font-medium">Bachelor of Science in Computer Science</p>
@@ -37,11 +37,11 @@ export default function Resume() {
                     <div>
                         Experience
                         <div className="pt-4 text-gray-300">
-                            <p className="text-xs">June  2021 - August 2021</p>
-                            <p className="text-sm font-bold pt-2 text-white">Beautycounter</p>
-                            <p className="text-xs pt-1">Frontend Engineer Intern</p>
-                            <ul className="text-xs pt-3 leading-relaxed">
-                                <li> - Implemented Javascript to Improve the Search Engine Optimization (SEO) performance for the company’s Ecommerce platform by returning correct status codes for nonexistent pages.</li>
+                            <p className="text-xs md:text-base">June  2021 - August 2021</p>
+                            <p className="text-sm md:text-lg font-bold pt-2 text-white">Beautycounter</p>
+                            <p className="text-xs md:text-base pt-1">Frontend Engineer Intern</p>
+                            <ul className="text-xs md:text-sm pt-3 leading-relaxed">
+                                <li> - Implemented Javascript to Improve the Search Engine Optimization (SEO) performance for the company’s Ecommerce platform.</li>
                                 <li className="pt-3"> - Assisted team of 6 by implementing and executing multiple unit tests across code base using Jest. </li>
                                 <li className="pt-3"> - Cross functional collaboration with design, backend and product teams to implement proper unit tests.</li>
                             </ul>
